fbfcdd2f9207d90bddbeef3687f33039c186a4f7
Hammsz <mhmmd07ilham@gmail.com>
2026-09-04 21:27:35 +0700

    Praktikum 1: Routing sederhana

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Nama: Muhammad Ilham Aulia Ferhat</p>
+      <p>Program Studi: Teknik Informatika</p>
+      <p>Universitas: Universitas Mercu Buana</p>
+    </div>
+  );
+}
